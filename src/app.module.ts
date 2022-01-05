770e9f41df82264bd8149897a56e53fd8ccc75df
creatong <jgh3482@gmail.com>
2022-01-06 02:29:13 +0900

    nestjs를 이용한 게시판

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BoardsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
