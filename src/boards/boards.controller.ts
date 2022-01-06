ba38cccebc095ecb7211ebadcd4bb89c3e54668d
creatong <jgh3482@gmail.com>
2022-01-07 02:02:57 +0900

    get, post controller, service 생성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { BoardsService} from './boards.service';
-
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto'
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService){}
 
-    @Get()
-    getAllBoard(){
+    @Get('/')
+    getAllBoard(): Board[]{
         return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto
+    ): Board{
+        return this.boardsService.createBoard(createBoardDto);
     }
 
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto'
 
 @Injectable()
 export class BoardsService {
-    private boards = [];
+    private boards: Board[] = [];
+    //다른 모듈에서 boards 수정하는 것을 차단하기 위해 private 사용
 
-    getAllBoards(){
+    getAllBoards(): Board[] {
+    //이런식으로 타입을 정의해주면 원하는 타입과 다른 코드가 사용되면 에러가 발생하게되고, 읽는 입장에서도 더 쉽게 이해가 가능하게 된다.
         return this.boards;
+    }
+
+    createBoard(createBoardDto: CreateBoardDto){
+        const {title, description} = createBoardDto;
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+        this.boards.push(board);
+        return board;
     }
 }
