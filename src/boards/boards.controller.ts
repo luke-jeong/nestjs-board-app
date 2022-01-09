dae41f9bfb5af6227914a41f3b1b0ab9615d885c
creatong <jgh3482@gmail.com>
2022-01-10 00:46:50 +0900

    유저 권한 설정, 유저와 게시물 관계 설정

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Delete, Patch, Body, Param, UsePipes, ValidationPipe, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Patch, Body, Param, UsePipes, ValidationPipe, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardStatus } from './board-status.enum';
 import { CreateBoardDto } from './dto/create-board.dto'
 import { Board } from './board.entity';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('boards')
+@UseGuards(AuthGuard())
 export class BoardsController {
     constructor(private boardsService: BoardsService){}
 
